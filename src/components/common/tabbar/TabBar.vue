f546683ab15b04cbb2fc7a5ca90e012b930f538f
回收彩电 <zhengyinting3323@163.com>
2022-07-28 10:55:10 +0800

    第二次初始化项目

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tabbar',
+  components: {},
+}
+</script>
+
+<style>
+@import 'assets/css/base.css';
+
+#tab-bar {
+  display: flex;
+  height: 49px;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #f6f6f6;
+  box-shadow: 0 -1px 1px rgba(1, 1, 1, 0.08);
+}
+</style>
---
+++ src/components/content/MainTabBar/MainTabBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home">
+      <img slot="item-icon" src="~assets/img/tabbar/homeNoClick.png" />
+      <img slot="item-icon-active" src="~assets/img/tabbar/home.png" />
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <img slot="item-icon" src="~assets/img/tabbar/classNoClick.png" />
+      <img slot="item-icon-active" src="~assets/img/tabbar/class.png" />
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="~assets/img/tabbar/shopCarNoClick.png" />
+      <img slot="item-icon-active" src="~assets/img/tabbar/shopCar.png" />
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="~assets/img/tabbar/mineNoClick.png" />
+      <img slot="item-icon-active" src="~assets/img/tabbar/mine.png" />
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+import TabBar from 'components/common/tabbar/TabBar.vue'
+import TabBarItem from 'components/common/tabbar/TabBarItem.vue'
+
+export default {
+  name: 'MainTabBar',
+  components: {
+    TabBar,
+    TabBarItem,
+  },
+}
+</script>
+
+<style></style>
